a5af1b649e988b7aa46c8869fdd70ca90cbea69c
Tech Aksh <techpune12@gmail.com>
2022-09-23 20:16:29 +0530

    initial

---
+++ src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	
+//	
+//		echo "# CICD-job2" >> README.md
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/akashsarwade/CICD-job2.git
+//		git push -u origin main
+//		…or push an existing repository from the command line
+//		git remote add origin https://github.com/akashsarwade/CICD-job2.git
+//		git branch -M main
+//		git push -u origin main
+	
+	}
+
+}
